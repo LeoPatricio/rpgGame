b36b1dc0c5f3228f0166576e0a3df98f658655dd
Leo Patricio <leo.ps9@hotmail.com>
2025-04-05 21:58:46 -0300

    Criação da janela princial do jogo

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Game {
+	requires java.desktop;
 }
--- src/rpgGame/Game.java
+++ src/rpgGame/Game.java
@@ -1,5 +1,39 @@
 package rpgGame;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+	
+	public static int WIDTH = 720;
+	public static int HEIGTH = 720;
+	
+	public Game () {
+		this.setPreferredSize(new Dimension(WIDTH, HEIGTH));
+	}
+	
+	public static void main (String[] args){
+		
+		Game game = new Game();
+		JFrame frame = new JFrame();
+		
+		frame.add(game);
+		frame.setTitle("rpg_Game");
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		
+		frame.setVisible(true);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+	@Override
+	public void run() {
+			
+		
+	}
 
 }
